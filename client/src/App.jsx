d8bc93b527250caf1347432c7b45a2b8b26ca289
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-10-04 16:26:19 +0100

    add a navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Signup from "./views/Signup";
 import Signin from "./views/Signin";
+import NavigationBar from "./components/NavigationBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <NavigationBar />
           <Route path="/" exact component={Home} />
           <Route path="/signin" exact component={Signin} />
           <Route path="/signup" exact component={Signup} />
