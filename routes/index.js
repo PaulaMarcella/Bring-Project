e900402cb70c8330bbdd591f178a4d7cc2957d4a
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-10-04 14:38:33 +0100

    solved some problems

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const Event = require('../models/event');
+// const Event = require('../models/event');
 
 module.exports = router;
