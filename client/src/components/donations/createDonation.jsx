bfd0cc726ee1e73b9e0a59d57aad59e69c33065c
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-10-04 16:45:39 +0100

    push

---
+++ client/src/components/donations/createDonation.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class createDonation extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create Donation Form here</h1>
+      </div>
+    );
+  }
+}
