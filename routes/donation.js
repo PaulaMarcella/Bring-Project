070840a26ebe19eb647ded4ac10ad6380145be69
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-10-04 14:35:41 +0100

    routes donation

--- routes/donation.js
+++ routes/donation.js
@@ -33,7 +33,7 @@ const checkLogin = require("../controllers/check-login");
 //   console.log(req.body);
 // });
 
-router.post("/donation", (req, res, next) => {
+router.post("/donation/create", (req, res, next) => {
   // Creating an event
   //console.log("The event object:", req.body);
   // const eventName = req.body.event;
@@ -53,11 +53,10 @@ router.post("/donation", (req, res, next) => {
     _creator
   })
     .then(donation => {
-      console.log(donation);
-      res.redirect("/donation" + donation._id);
+      res.json({ type: "success", data: { donation } });
     })
     .catch(error => {
-      console.log(error);
+      next(error);
     });
 });
 
@@ -66,94 +65,81 @@ router.post("/donation", (req, res, next) => {
 
 router.get("/donation/:id", checkLogin, (req, res, next) => {
   Donation.findById(req.params.id)
-    .populate("creator")
+    .populate("_creator")
     .then(donation => {
-      res.render("event/eventPage", { donation });
+      res.json({ type: "success", data: { donation } });
     })
     .catch(error => {
-      console.log(error);
+      next(error);
     });
 });
 
-router.get("/donation/:id/edit", checkLogin, (req, res, next) => {
-  // console.log(req.params.id);
-  Donation.findById(req.params.id)
-    .then(donation => {
-      // console.log(event)
-      res.render("event/eventPage-edit", { donation });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
-router.post("/donation/:id/edit", (req, res, next) => {
-  const eventName = req.body.event;
-  const description = req.body.description;
-  const artists = req.body.artists;
-  const genre = req.body.genre;
-  const city = req.body.city;
-  const ticketURL = req.body.ticket;
-  const date = req.body.date;
-  const eventId = req.params.id;
-
-  const data = {
-    eventName,
+router.patch("/donation/:id/edit", (req, res, next) => {
+  const {
+    donationName,
+    category,
     description,
-    artists,
-    genre,
-    city,
-    ticketURL,
-    date
-  };
-  //console.log("DATA TO BE EDIT", data)
+    location,
+    imageUrl,
+    _creator
+  } = req.body;
+  const donationId = req.params.id;
 
-  Donation.findByIdAndUpdate(eventId, data)
-    .populate("creator")
-    .then(event => {
-      console.log(event);
-      res.redirect("/eventPage/" + eventId);
+  Donation.findByIdAndUpdate(
+    donationId,
+    {
+      ...(donationName && { donationName }),
+      ...(category && { category }),
+      ...(description && { description }),
+      ...(location && { location }),
+      ...(imageUrl && { imageUrl }),
+      ...(_creator && { _creator })
+    },
+    { new: true }
+  )
+    .then(donation => {
+      res.json({ type: "success", data: { donation } });
     })
     .catch(error => {
-      console.log("Could not update event information", error);
+      next(error);
     });
 });
 
-router.get("/donation/:id/delete", (req, res, next) => {
-  const eventId = req.params.id;
+router.delete("/donation/:id/delete", (req, res, next) => {
+  const donationId = req.params.id;
   // Grab the ID and use it as an argument for deleting
-  Donation.findByIdAndDelete(eventId)
+  Donation.findByIdAndDelete(donationId)
     .then(() => {
-      res.redirect("/");
+      res.json({ type: "success" });
     })
     .catch(error => {
-      console.log(error);
+      next(error);
     });
 });
 
-router.post("/add-comment/:id", checkLogin, (req, res, next) => {
-  const commentBody = req.body.commentbody;
-  const commentTitle = req.body.commenttitle;
-  const commentAuthor = req.session.user._id;
-  const eventId = req.params.id;
+// router.post("/donation/:id", checkLogin, (req, res, next) => {
+//   const commentBody = req.body.commentbody;
+//   const commentTitle = req.body.commenttitle;
+//   const commentAuthor = req.session.user._id;
+//   const eventId = req.params.id;
 
-  // console.log("COMMENT DATA", data);
-  Donation.findByIdAndUpdate(eventId, {
-    $push: {
-      comments: {
-        commentBody,
-        commentTitle,
-        commentAuthor
-      }
-    }
-  })
-    .then(() => {
-      res.redirect("/donation/" + eventId);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+//   // console.log("COMMENT DATA", data);
+//   Donation.findByIdAndUpdate(eventId, {
+//     $push: {
+//       comments: {
+//         commentBody,
+//         commentTitle,
+//         commentAuthor
+//       }
+//     }
+//   })
+//     .then(() => {
+//       res.redirect("/donation/" + eventId);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// });
 
 // router.get('/add-comment/:id', checkLogin, (req, res, next) => {
 //   const eventId= req.params.id;
@@ -167,31 +153,31 @@ router.post("/add-comment/:id", checkLogin, (req, res, next) => {
 //   });
 // });
 
-router.get("/donation-browse", (req, res, next) => {
-  let searchResult;
-  if (req.query.search) {
-    searchResult =
-      req.query.search.charAt(0).toUpperCase() +
-      req.query.search.slice(1).toLowerCase();
-  }
-  //console.log("search result",searchResult);
-  const typeResult = req.query.type;
+// router.get("/donation-browse", (req, res, next) => {
+//   let searchResult;
+//   if (req.query.search) {
+//     searchResult =
+//       req.query.search.charAt(0).toUpperCase() +
+//       req.query.search.slice(1).toLowerCase();
+//   }
+//   //console.log("search result",searchResult);
+//   const typeResult = req.query.type;
 
-  Donation.find({})
-    .then(allEvents => {
-      // console.log("ALL EVENTS",allEvents);
-      return allEvents.filter(event => event[typeResult] === searchResult);
-    })
-    .then(eventList => {
-      // console.log("FILTERED EVENTS",eventList);
-      const data = {
-        eventList
-      };
-      res.render("browse", data);
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+//   Donation.find({})
+//     .then(allEvents => {
+//       // console.log("ALL EVENTS",allEvents);
+//       return allEvents.filter(event => event[typeResult] === searchResult);
+//     })
+//     .then(eventList => {
+//       // console.log("FILTERED EVENTS",eventList);
+//       const data = {
+//         eventList
+//       };
+//       res.render("browse", data);
+//     })
+//     .catch(error => {
+//       next(error);
+//     });
+// });
 
 module.exports = router;
