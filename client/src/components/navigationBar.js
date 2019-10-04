24433317523803eb041c6c54953c924eadd51c88
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-10-04 16:16:54 +0100

    navbar

--- client/src/components/navigationBar.js
+++ client/src/components/navigationBar.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import "";
+import Navbar from "react-bootstrap/Navbar";
 
 export default class navigationBar extends Component {
   render() {
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Bring</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href={Signin}>Sign in</Nav.Link>
+              <Nav.Link href={Signup}>Sign Up</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
